47446610ac5ad89c0ba6db38a2b2190ad60004b2
Gabriel <smecrowl9@gmail.com>
2025-06-25 17:49:37 -0300

    Add core classes for game board functionality

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,12 @@
+package application;
+
+import BoardGame.Board;
+import BoardGame.Position;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        Board board = new Board(8, 8);
     }
 }
